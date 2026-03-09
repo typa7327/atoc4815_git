398a12746591a1e2d789acf964543afc8f538025
Tyler Pachuda <typa7327@colorado.edu>
2026-03-09 14:12:43 -0600

    Added my name to boulder analysis

--- boulder_temperatures.py
+++ boulder_temperatures.py
@@ -10,7 +10,7 @@ Instructions:
 """
 
 # TODO: Add your name here
-STUDENT_NAME = "Will Chapman"  # Example: "Alice Johnson"
+STUDENT_NAME = "Tyler Pachuda"  # Example: "Alice Johnson"
 
 
 def load_temperatures(filepath):
